0bd5625a26a056cceab8d3a5314ef0a5fb774f8e
BharatKumarNaik <88843733+BharatKumarNaik@users.noreply.github.com>
2024-02-09 11:10:25 +0530

    Update simple_web_App.js

--- simple_web_App.js
+++ simple_web_App.js
@@ -14,6 +14,7 @@ const server = http.createServer((req, res) => {
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1 (localhost)
+const port=process.env.PORT || 3000;
 server.listen(3000, () => {
   console.log('Server running at http://127.0.0.1:3000/');
 });
