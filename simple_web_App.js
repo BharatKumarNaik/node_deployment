f801829ec132ca844b4112d8a8aa8b4ae60379c4
BharatKumarNaik <88843733+BharatKumarNaik@users.noreply.github.com>
2024-02-09 09:10:30 +0530

    Add files via upload

---
+++ simple_web_App.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+ fs.readFile('home.html', (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.end("Error: File Not Found");
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        }
+    });
+});
+
+// Listen on port 3000, IP defaults to 127.0.0.1 (localhost)
+server.listen(3000, () => {
+  console.log('Server running at http://127.0.0.1:3000/');
+});
